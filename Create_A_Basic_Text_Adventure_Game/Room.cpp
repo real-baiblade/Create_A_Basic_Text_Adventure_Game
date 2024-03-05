4279c7776c4c0075142e383d38e3e0c1d210a107
real-baiblade <baiden.harrod@gmail.com>
2024-03-05 20:07:58 +1100

    implemented Room class functions

--- Create_A_Basic_Text_Adventure_Game/Room.cpp
+++ Create_A_Basic_Text_Adventure_Game/Room.cpp
@@ -1,1 +1,17 @@
 #include "Room.h"
+#include "String.h"
+#include <iostream>
+using namespace std;
+
+Room::Room(String description, Item* item) {
+	Room::description = description;
+	Room::item = item;
+}
+
+Room::~Room() {
+
+}
+
+void Room::Description() const {
+	cout << description.CStr() << endl;
+}
